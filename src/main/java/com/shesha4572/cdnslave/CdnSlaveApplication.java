5a7b540dd8e0fed9e579ec9f7e73a34e97aafb04
shesha4572 <shesha4572@gmail.com>
2023-09-30 21:46:56 +0530

    Initial Commit

---
+++ src/main/java/com/shesha4572/cdnslave/CdnSlaveApplication.java
@@ -0,0 +1,13 @@
+package com.shesha4572.cdnslave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdnSlaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdnSlaveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shesha4572/cdnslave/CdnSlaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shesha4572.cdnslave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdnSlaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
